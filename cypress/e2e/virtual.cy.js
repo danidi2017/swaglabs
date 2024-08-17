1b862688405eff5b80af1d193fe0145aeae7bf5e
hernanj.romerov <hernanj.romerov@autonoma.edu.co>
2024-08-17 09:41:45 -0500

    Implementación Usuarios Válidos/Inválidos

--- cypress/e2e/virtual.cy.js
+++ cypress/e2e/virtual.cy.js
@@ -4,9 +4,9 @@ describe('Proceso de compra', () => {
     cy.get(".login_logo").should("be.visible")
   })
 
-const users = Cypress.env('users');
+const valid_users = Cypress.env('valid_users');
 
-Object.entries(users).forEach(([username, password]) => {
+Object.entries(valid_users).forEach(([username, password]) => {
   it('Validar inicio de sesion', () => {
     cy.visit(Cypress.env('base_url'))
     cy.get('[data-test="username"]').type(username)
@@ -15,13 +15,17 @@ Object.entries(users).forEach(([username, password]) => {
   })
 })
 
+const invalid_users = Cypress.env('invalid_users');
+
+Object.entries(invalid_users).forEach(([username, password]) => {
   it('Validar password invalido', () => {
     cy.visit(Cypress.env('base_url'))
-    cy.get('[data-test="username"]').type("standard_user")
-    cy.get('[data-test="password"]').type("12345")
+    cy.get('[data-test="username"]').type(username)
+    cy.get('[data-test="password"]').type(password)
     cy.get('[data-test="login-button"]').click()
     cy.get('[data-test="error"]').contains("Epic sadface: Username and password do not match any user in this service")
   })
+})
 
 })
 
